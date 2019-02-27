0422b401ead8e315d5a58ef573928b453e31e43d
Amira <amira@drieam.nl>
2019-02-27 17:15:48 +0100

    add lead resource, not tested yet

---
+++ src/Resources/Label.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 363690
+ * Date: 27/02/2019
+ * Time: 14:32
+ */
+
+namespace Eduframe\Resources;
+
+use Eduframe\Resource;
+use Eduframe\Traits\FindAll;
+use Eduframe\Traits\FindOne;
+
+
+class Label extends Resource {
+
+    use FindAll, FindOne;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'updated_at',
+        'created_at',
+        'color',
+        'model_type'
+    ];
+
+
+    /**
+     * @var string
+     */
+    protected $model_name = 'Labels';
+
+    /**
+     * @var string
+     */
+    protected $endpoint = 'labels';
+
+    /**
+     * @var string
+     */
+    protected $namespace = 'course';
+
+}
