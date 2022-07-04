6fcacb3ca89a4b630639e2543b6ac91f7ea2b041
Stijn Nooijen <s.j.m.nooijen@gmail.com>
2022-07-04 14:08:38 +0200

    feat: Add catalog products and variants endpoints
    We want to add catalog products and variants to the wordpress plugin, so we also need to add it to this php library.

---
+++ src/Resources/CatalogVariant.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Eduframe\Resources;
+
+use Eduframe\Resource;
+use Eduframe\Traits\FindAll;
+use Eduframe\Traits\FindOne;
+
+class CatalogVariant extends Resource {
+    use FindAll, FindOne;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'product_id',
+        'variantable_type',
+        'variantable_id',
+        'is_published',
+        'cost_scheme',
+        'cost',
+        'availability',
+        'updated_at',
+        'created_at',
+        'available_places',
+        'currency'
+    ];
+
+    /**
+     * @var string
+     */
+    protected $model_name = 'CatalogVariant';
+
+    /**
+     * @var string
+     */
+    protected $endpoint = 'catalog/variants';
+
+    /**
+     * @var string
+     */
+    protected $namespace = 'catalog_variant';
+}
